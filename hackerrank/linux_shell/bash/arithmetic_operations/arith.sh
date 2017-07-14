4994760cd245584a72e0a2965903eaa1b3c27b5d
bdnl <ricardo.chagas2@outlook.com>
2017-07-14 19:14:47 -0300

    Give exercise #9 a proper solution

--- hackerrank/linux_shell/bash/arithmetic_operations/arith.sh
+++ hackerrank/linux_shell/bash/arithmetic_operations/arith.sh
@@ -2,9 +2,4 @@
 
 read exp
 
-if [ "$exp" = "5+50*3/20 + (19*2)/7" ]
-then
-    echo "scale=3; $exp + 0.001" | bc -l
-else 
-    echo "scale=3; $exp" | bc -l
-fi
+printf "%.3f" $( echo "$exp" | bc -l )
