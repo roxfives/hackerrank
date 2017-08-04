afee30d35605e129d9f24c5e0d33bdc342ea31c9
bdnl <ricardo.chagas2@outlook.com>
2017-08-04 18:11:53 -0300

    Gives a much more concise answer to arrays_in_bash#7

--- hackerrank/linux_shell/arrays_in_bash/remove_the_first_capital_letter/cap_letter.sh
+++ hackerrank/linux_shell/arrays_in_bash/remove_the_first_capital_letter/cap_letter.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
 array=($(cat /dev/stdin))
-for i in $(seq 0 $((${#array[@]} - 1)) ); do
-    if [[ "${array[$i]:0:1}" =~ [A-Z] ]]; then
-       array[$i]=".${array[$i]:1}"
-    fi
-done
-
-echo ${array[@]}
+echo ${array[@]/[A-Z]/.}
